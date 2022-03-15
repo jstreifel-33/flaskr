0536ca61866a00f9cc930ed719a317209c105b46
Joe Streifel <josephstreifel@gmail.com>
2022-03-14 21:35:38 -0700

    creates function for retrieving posts from db

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,7 +6,26 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+
 bp = Blueprint('blog', __name__)
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
 
 @bp.route('/')
 def index():
@@ -17,6 +36,7 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
